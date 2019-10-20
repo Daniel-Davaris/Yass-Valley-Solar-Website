f782434cc0613640a56d2e9877e18ec50635d849
daniel davaris <danieldavarispriv@gmail.com>
2019-10-20 21:47:58 +1100

    implemented everything to extend to base with external footer, header ect..

---
+++ static/js/main.js
@@ -0,0 +1,94 @@
+$(document).ready(function() {
+    // console.log("worked")
+   
+    
+    // $('.carousel').carousel({
+    //     interval: 3000
+    //   })
+    // alert("iii")
+    $("window").scroll(function() {
+        alert("yesss boiiiu")
+        console.log("yes")
+        // $(".navbar-collapse").toggleClass("show");
+        // $(".navbar-toggler").toggleClass("collapsed");
+
+    });
+
+    $(".clr").mouseenter(function() {
+        
+        // alert("start")
+        $(".nav-container").toggleClass("grad");
+        $(".nav-container").toggleClass("clicked");
+
+    });
+
+    $(".clr").mouseleave(function() {
+        $(".nav-container").toggleClass("clicked");
+        $(".nav-container").toggleClass("grad");
+        
+    });
+});
+
+// console.log("new")
+// function resize() {
+//     if ($(window).width() < 1200) {
+//      console.log("yeah boi")
+//      $('#chill').addClass('col-5');
+//     }
+//     else {$('#chill').removeClass('col-5');}
+// }
+
+// $(document).ready( function() {
+//     $(window).resize(resize);
+//     resize();
+// });
+
+
+$(document).click(function() {
+    // alert('clicked outside');
+    
+    if ($(".navbar-collapse").hasClass("show")) {
+        // alert("has show ")
+        $(".navbar-collapse").toggleClass("show");
+    }
+});
+
+
+// $(document).click(function() {
+//     alert('clicked outside');
+
+// });
+
+// $(".navbar-collapse").click(function(event) {
+//     $(".navbar-collapse").toggleClass("show");
+//     $(".navbar-toggler").toggleClass("collapsed");
+//     // alert('clicked inside');
+//     event.stopPropagation();
+// });
+
+// $(document).click(function(event) { 
+//     $target = $(event.target);
+//     if(!$target.closest('#navbar-collapse').length && 
+//     $('#navbar-collapse').is(":visible")) {
+//       $('#navbar-collapse').hide();
+//     }        
+// });
+// $(document).click(function(event) { 
+//     $target = $(event.target);
+//     if(!$target.closest('#navbar-toggler').length && 
+//     $('#navbar-toggler').is(":visible")) {
+//       $('#navbar-toggler').hide();
+//     }        
+// });
+
+// document.getElementById("navbar").onmouseover = function () { mouseOver() };
+// document.getElementById("navbar").onmouseout = function () { mouseOut() };
+
+// function mouseOver() {
+//     document.getElementById("navbar").setAttribute("style", "background-color: rgba(0, 0, 0, 0.47)");
+
+// }
+
+// function mouseOut() {
+//     document.getElementById("navbar").setAttribute("style", "background-color: rgba(193, 66, 66, 0)");
+// }
