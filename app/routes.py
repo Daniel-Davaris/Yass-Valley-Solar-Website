8626ebf8c337d1f986f3e648695ef3d245906836
daniel davaris <danieldavarispriv@gmail.com>
2019-11-26 11:20:26 +1100

    Bettery Storage section on 'Services' page

--- app/routes.py
+++ app/routes.py
@@ -48,7 +48,6 @@ def blog():
                            next_url=next_url, prev_url=prev_url)
 
 
-
 @app.route('/our_team')
 # @login_required
 def our_team():
@@ -116,7 +115,7 @@ def my_posts():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('my_posts'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -126,7 +125,7 @@ def login():
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
+            next_page = url_for('my_posts')
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
